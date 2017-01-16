83c04824ccb95adcb6c7b9fc2026866183278ecb
Mark Antipov <oper29a@gmail.com>
2017-01-16 15:03:49 +0500

    Publishable filter.

---
+++ src/Annotation/Publishable.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Everus\DoctrineFilters\Annotation;
+
+use Doctrine\Common\Annotations\Annotation;
+
+/**
+ * Class Publishable
+ * @package Everus\DoctrineFilters\Annotation
+ * @Annotation
+ * @Target("CLASS")
+ */
+
+class Publishable
+{
+
+}
---
+++ src/Filter/PublishedFilter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Everus\DoctrineFilters\Filter;
+
+
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\Query\Filter\SQLFilter;
+use Doctrine\Common\Annotations\Reader;
+
+class PublishedFilter extends SQLFilter
+{
+    /**
+     * @var Reader
+     */
+    protected $reader;
+
+    public function setAnnotationReader(Reader $reader)
+    {
+        $this->reader = $reader;
+    }
+
+    public function addFilterConstraint(ClassMetadata $targetEntity, $targetTableAlias)
+    {
+        if (empty($this->reader)) {
+            return '';
+        }
+        
+        $publishable = $this->reader->getClassAnnotation(
+            $targetEntity->getReflectionClass(),
+            'Everus\\DoctrineFilters\\Annotation\\Publishable'
+        );
+
+        if (!$publishable) {
+            return '';
+        }
+
+        return $targetTableAlias.'.published = true';
+    }
+}
